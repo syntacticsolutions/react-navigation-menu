a64b4f0ad46a68973b246f3b145de9d2ba62fbae
Miguel Coder <syntacticsolutions@gmail.com>
2019-08-03 19:35:19 -0700

    Now using react-hooks and functional components

--- src/App.js
+++ src/App.js
@@ -1,54 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Toolbar from './components/Toolbar'
 import Contact from './components/Contact'
 import { Router } from '@reach/router'
 import './App.css';
 import './assets/scss/base.scss'
 
-class App extends Component {
-  constructor (props) {
-    super(props)
+function App() {
+    const navLinks = [
+      {
+        text: 'Contact',
+        path: '/contact',
+        icon: 'ion-ios-megaphone'
+      }
+      // {
+      //   text: 'About',
+      //   path: '/about',
+      //   icon: 'ion-ios-business'
+      // },
+      // {
+      //   text: 'Blog',
+      //   path: '/blog',
+      //   icon: 'ion-ios-bonfire'
+      // },
+      // {
+      //   text: 'Portfolio',
+      //   path: '/portfolio',
+      //   icon: 'ion-ios-briefcase'
+      // }
+    ]
 
-    this.state = {
-      navLinks: [
-        {
-          text: 'Contact',
-          path: '/contact',
-          icon: 'ion-ios-megaphone'
-        }
-        // {
-        //   text: 'About',
-        //   path: '/about',
-        //   icon: 'ion-ios-business'
-        // },
-        // {
-        //   text: 'Blog',
-        //   path: '/blog',
-        //   icon: 'ion-ios-bonfire'
-        // },
-        // {
-        //   text: 'Portfolio',
-        //   path: '/portfolio',
-        //   icon: 'ion-ios-briefcase'
-        // }
-      ]
-    }
-  }
-
-  render () {
-    let { navLinks } = this.state
     return (
       <div className="App">
           <Toolbar
             navLinks={ navLinks }
-            imagePath={require('./logo.svg')}
+            imagePath={ require('./logo.svg') }
           ></Toolbar>
           <Router>
             <Contact path="/contact" />
           </Router>
       </div>
-    );
-  }
+    )
 }
 
 export default App;
--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,55 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from "@reach/router";
 
-class Toolbar extends Component {
-    constructor (props) {
-        super(props)
-        this.state = { 
-            navOpen: false,
-            hoverIndex: -1
-        }
-    }
+function Toolbar({ background, hoverBackground, linkColor, navLinks, imagePath }) {
+    const [ navOpen, setNavOpen ] = useState(0)
+    const [ hoverIndex, setHoverIndex ] = useState(-1)
 
-    set = (key, payload) => {
-        this.setState({ [key]: payload })
-    }
-
-    render() {
-        const { background, hoverBackground, linkColor, navLinks, imagePath } = this.props;
-        const { hoverIndex, navOpen } = this.state
-        return (
-            <nav style={{ background: background || '#333' }}>
-                <ul
-                    style={{ background: background || '#333' }}
-                    class={ navOpen ? 'active' : '' }
-                >
-                    <figure class="image-logo" onClick={ () => { this.set('navOpen', !navOpen) } }>
-                        <img src={ imagePath } height="40px" width="40px" alt="toolbar-logo" />
-                    </figure>
-                    {   
-                        navLinks.map((link, index) => 
-                            <li
-                                onMouseEnter={ () => { this.set('hoverIndex', index) } }
-                                onMouseLeave={ () => { this.set('hoverIndex', -1) } }
-                                style={{ background: hoverIndex !== index ? (background || '#333') : (hoverBackground || '#999') }}
-                            >
-                                <Link
-                                    to={link.path}
-                                    style={{ color: linkColor || '#DDD' }}
-                                >
-                                    { link.text }
-                                    <i className={ link.icon } />
-                                </Link>
-                            </li>
-                        )
-                    }
-                </ul>
-            </nav>
-        )
-    }
+    return (
+        <nav
+            class="responsive-toolbar"
+            style={{ background: background || '#333' }}>
+            <ul
+                style={{ background: background || '#333' }}
+                className={ navOpen ? 'active' : '' }
+            >
+                <figure class="image-logo" onClick={ () => { setNavOpen(!navOpen) } }>
+                    <img src={ imagePath } height="40px" width="40px" alt="toolbar-logo" />
+                </figure>
+                { navLinks.map((link, index) => 
+                    <li
+                        onMouseEnter={ () => { setHoverIndex(index) } }
+                        onMouseLeave={ () => { setHoverIndex(-1) } }
+                        style={{ background: hoverIndex !== index ? (background || '#333') : (hoverBackground || '#999') }}
+                    >
+                        <Link
+                            to={link.path}
+                            style={{ color: linkColor || '#DDD' }}
+                        >
+                            { link.text }
+                            <i className={ link.icon } />
+                        </Link>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    )
 }
-
 
 Toolbar.propTypes = {
     navLinks: PropTypes.array.isRequired,
